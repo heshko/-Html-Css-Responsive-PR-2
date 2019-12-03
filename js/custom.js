e131b0eb6c9f430623c40bf3a89da5e4c1e132d9
Heshko <shooter.metall@gmail.com>
2019-12-03 20:12:58 +0100

    Responsive website with my frameWork

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+$(function(){
+    $(".info-list li ").click(function(){
+        $(this).addClass("selected").siblings("li").removeClass("selected");
+        $(".content-info div").hide();
+        $("."+$(this).data("class")).fadeIn();
+    })
+})
+
